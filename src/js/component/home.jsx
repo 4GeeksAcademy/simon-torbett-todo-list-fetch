0db21ff4e400116dd469317eb8f7ebdb0a5bf35c
Storbett21 <96504042+Storbett21@users.noreply.github.com>
2023-10-08 17:41:00 +0000

    second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,109 +1,92 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
 const Home = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [todos, setTodos] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+	const [inputValue, setInputValue] = useState("")
+	const [todos, setTodos] = useState([])
+	const [isLoading, setIsLoading] = useState(true)
 
-  useEffect(() => {
-    // Realizar la llamada fetch al montar el componente
-    fetch("https://playground.4geeks.com/apis/fake/todos/simontorbett")
-      .then(response => response.json())
-      .then(data => {
-        setTodos(data);
-        setIsLoading(false);
-      })
-      .catch(error => {
-        console.error('Error:', error);
-        setIsLoading(false);
-      });
-  }, []);
+	useEffect(() => {
+		fetch("https://playground.4geeks.com/apis/fake/todos/")
+			.then(response => response.json())
+			.then(data => {
+				setTodos(data)
+				setIsLoading(false)
+			})
+			.catch(error => {
+				console.log('Error', error)
+				setIsLoading(false)
+			})
+	}, [])
+	const addtodo = () => {
+		if (inputValue.trim() !== '') {
+			const newTodo = { label: inputValue, done: false }
+			setTodos([...todos, newTodo])
 
-  const addTodo = () => {
-    if (inputValue.trim() !== '') {
-      const newTodo = { label: inputValue, done: false };
-      // Agregar el nuevo TODO en el frontend
-      setTodos([...todos, newTodo]);
+			updateBackendTodos([...todos, newTodo])
+			setInputValue('')
+		}
+	}
+	const deleteTodo = (index) => {
+		const updateTodos = todos.filter((currentIndex) => index !== currentIndex)
+		setTodos(updateTodos)
+		updateBackendTodos(updateTodos)
+	}
+	const updateBackendTodos = (updateTodos) => {
+		fetch("https://playground.4geeks.com/apis/fake/todos/", {
+			method: 'PUT',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(updateTodos)
+		})
+			.then(response => response.json())
+			.then (data => console.log(data))
+			.catch(error => console.error('Error:', error))
+	}
+	const ClearAllTodos = () => {
+		setTodos([])
+		fetch("https://playground.4geeks.com/apis/fake/todos/", {
+			method: 'DELETE'
+		})
+			.then(response => response.json())
+			.then(data=>console.log(data))
+			.catch(error => console.error('Error:', error))
+	}
+	return (
+		<div className="text-center"> <h1 className="titulo">Todos</h1>
 
-      // Actualizar el backend con la nueva lista de TODOs
-      updateBackendTodos([...todos, newTodo]);
-      
-      setInputValue('');
-    }
-  };
+			<ul className="list-group list-group-flush">
+				<li className="list-group-item" >
+					<input
+						type="text"
+						onChange={(e) => setInputValue(e.target.value)}
+						value={inputValue}
+						onKeyDown={(e) => {
+							if (e.key === "Enter") {
+								addtodo();
+							}
+						}}
+						placeholder='Que Necesitas Hacer' />
+				</li>
+				{isLoading ? (<li className='list-group-item'>cargando</li>):(
+				todos.map((item, index) => (
+					<li className="list-group-item" key={index}> {item.label} <FontAwesomeIcon icon={faTrashCan} onClick={() => deleteTodo(index) } /></li>
+				))
+				)}
+			</ul>
+			<div className='tareas'>{todos.length}</div>
+			<button onClick={ClearAllTodos}>Limpiar </button>
 
-  const deleteTodo = (index) => {
-    // Eliminar el TODO en el frontend
-    const updatedTodos = todos.filter((_, currentIndex) => index !== currentIndex);
-    setTodos(updatedTodos);
-
-    // Actualizar el backend con la nueva lista de TODOs
-    updateBackendTodos(updatedTodos);
-  };
-
-  const updateBackendTodos = (updatedTodos) => {
-    // Actualizar el backend con la nueva lista de TODOs
-    fetch("https://playground.4geeks.com/apis/fake/todos/simontorbett", {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(updatedTodos),
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
-  };
-
-  const clearAllTodos = () => {
-    // Limpiar todos los TODOs en el frontend
-    setTodos([]);
-
-    // Limpiar todos los TODOs en el backend
-    fetch("https://playground.4geeks.com/apis/fake/todos/simontorbett", {
-      method: 'DELETE',
-    })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Error:', error));
-  };
-
-  return (
-    <div className="text-center">
-      <h1 className="titulo">Todos</h1>
-
-      <ul className="list-group list-group-flush">
-        <li className="list-group-item">
-          <input
-            type="text"
-            onChange={(e) => setInputValue(e.target.value)}
-            value={inputValue}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") {
-                addTodo();
-              }
-            }}
-            placeholder='¿Qué necesitas hacer?' />
-        </li>
-        {isLoading ? (
-          <li className="list-group-item">Cargando...</li>
-        ) : (
-          todos.map((item, index) => (
-            <li className="list-group-item" key={index}>
-              {item.label}
-              <FontAwesomeIcon icon={faTrashCan} onClick={() => deleteTodo(index)} />
-            </li>
-          ))
-        )}
-      </ul>
-      <div className='tareas'>{todos.length}</div>
-
-      <button onClick={clearAllTodos}>Limpiar Todas las Tareas</button>
-    </div>
-  );
+		</div>
+	);
 };
 
 export default Home;
